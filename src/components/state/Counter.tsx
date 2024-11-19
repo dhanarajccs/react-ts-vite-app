14a774c39bb9609ee1eee609a5c0ec8fe86d35c4
Dhanaraj Narsimmareddy <dhanarajnarasimmareddy@coatesgroup.com>
2024-11-19 12:25:24 +0530

    feat: ts with useReducer hook

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -1,3 +1,4 @@
+/*
 import { useReducer } from 'react' 
 
 // 1. initialState as object
@@ -30,6 +31,8 @@ export const Counter = () => {
     </>
   ) 
 } 
+*/
+
 
 /*
 In a simple counter component, the useReducer hook is used to maintain the count state and update to the counter value 
@@ -52,3 +55,47 @@ we've called the useReducer hook, passing in the reducer function and the initia
 increment button is to update the count value which on click dispatches an action where type is increment and payload is 10. 
 decrement button is to update the count value which on click dispatches an action where type is decrement and payload is 10. 
 */
+
+import { useReducer } from 'react' 
+
+// 4. Type for state object
+type CounterState = {
+  count: number
+}
+
+// 5. Type for action object
+type CounterAction = {
+  type: string
+  payload: number
+}  
+
+// 1. initialState as object
+const initialState = { count: 0 } 
+
+// 2. reducer function
+function reducer(state: CounterState, action: CounterAction) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.payload } 
+    case 'decrement':
+      return { count: state.count - action.payload } 
+    default:
+      return state 
+  }
+} 
+
+// 3. Counter component using useReducer hook
+export const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState) 
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
+        Decrement 10
+      </button>
+    </>
+  ) 
+} 
